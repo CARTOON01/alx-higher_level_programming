0af13dc6e557087e5b203ba3d68d0fde8ee7e4c6
CARTOON01 <brianngugi224@gmail.com>
2023-05-09 15:09:29 -0700

    11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,22 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    # If b is 0, return 1
-    if b == 0:
-        return 1
-
-    # If a is negative and b is even, return NaN
-    if a < 0 and b % 2 == 0:
-        return float('nan')
-
-    # Compute the power using repeated multiplication
-    result = 1
-    abs_b = abs(b)
-    for i in range(abs_b):
-        result *= a
-
-    # If b is negative, return 1/result
-    if b < 0:
-        return 1/result
-
-    # Otherwise, return the result
-    return result
+    return (a ** b)
