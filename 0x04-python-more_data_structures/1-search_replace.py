6132004264d1b64e97a5abbdb38f52aac8e72155
CARTOON01 <brianngugi224@gmail.com>
2023-05-17 22:48:45 +0000

    task 2

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,6 @@
 def search_replace(my_list, search, replace):
     # Create a new list to store the modified elements
     new_list = []
-
     # Iterate over each element in the input list
     for item in my_list:
         # If the element matches the search element, replace it with the new element
