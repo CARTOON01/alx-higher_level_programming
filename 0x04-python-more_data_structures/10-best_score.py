996c4822e07c2357eafb9a9f747b84d0ace56c95
CARTOON01 <brianngugi224@gmail.com>
2023-05-17 23:16:46 +0000

    update on task 11

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    # Initialize variables to keep track of the best score and key
-    best_key = None
-    best_score = float('-inf')  # Start with negative infinity as the initial score
+    if not a_dictionary:
+        return (None)
 
-    # Iterate over each key-value pair in the dictionary
-    if a_dictionary is not None:
-        for key, value in a_dictionary.items():
-            # If the current value is greater than the best score, update the best score and key
-            if value > best_score:
-                best_score = value
-                best_key = key
-
-    return best_key
+    return (max(a_dictionary, key=a_dictionary.get))
