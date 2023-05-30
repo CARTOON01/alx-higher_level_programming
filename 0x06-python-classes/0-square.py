87f14dda91e54c0849acdcd7b6fdfa371acd7d97
Brian Ngugi <brianngugi224@gmail.com>
2023-05-31 02:18:09 +0300

    update on task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,5 +3,16 @@
 """This module defines a Square class"""
 
 class Square:
-    """This class is an empty Square class"""
-    pass
+    """This class represents a square"""
+
+    def __init__(self, side_length):
+        """Initialize the Square object with a given side length"""
+        self.side_length = side_length
+
+    def area(self):
+        """Calculate and return the area of the square"""
+        return self.side_length ** 2
+
+    def perimeter(self):
+        """Calculate and return the perimeter of the square"""
+        return 4 * self.side_length
