ef4b76808204f84b647847b23593b1dc4ca4e2d2
CARTOON01 <brianngugi224@gmail.com>
2023-09-08 13:54:09 -0700

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,19 +1,6 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const array = process.argv;
-let data;
-fs.readFile(`./${array[2]}`, 'utf-8', (err, d) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  data = d;
-  fs.readFile(`./${array[3]}`, 'utf-8', (err, d) => {
-    if (err) throw err;
-    data += d;
-    fs.writeFile(`./${array[4]}`, data, (err) => {
-      if (err) throw err;
-    });
-  });
-});
+
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
